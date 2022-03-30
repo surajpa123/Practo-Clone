8e26b3b8638e89a0d32d92c0a30c3fcbed3bb298
Ashiq Khan <khanashiq4494@gmail.com>
2022-03-30 13:22:45 +0530

    navbar and footer

---
+++ practo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbarwrapper from './components/Navbar/Navbarwrapper';
+import Footerwrapper from './components/Footer/Footerwrapper';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbarwrapper />
+        <Footerwrapper />
+    </div>
+  );
+}
+
+export default App;
