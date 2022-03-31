235dfe03c46e4d6421b9dc7bc0ae5e1b3802821b
surajpa123 <77233053+surajpa123@users.noreply.github.com>
2022-03-31 13:07:59 +0530

    updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import {Consult} from "./Components/Consult"
-
+import {Benifit} from "./Components/Benifit"
+import {Faq} from "./Components/Faq"
+import {Health} from "./Components/Health"
+import {Appdownload} from "./Components/Appdownload"
+import {Finddoctors} from "./Components/Finddoctors"
+import {Routes,Route} from "react-router-dom"
+// import {Footerwrapper} from "./Components/Footerwrapper"
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      
-<Consult/>
+<div className="App">  
+<Benifit/>
+<Faq/>
+<Health/>
+<Appdownload/>
+<Routes>
 
+<Route path='/find' element= {<Finddoctors/>}></Route>
 
-    </div>
+</Routes>
+</div>
   )
 }
 
---
+++ vite-project/src/Components/Finddoctors.jsx
@@ -0,0 +1,10 @@
+  import React from 'react'
+  
+  export const Finddoctors=()=> {
+    return (
+        <div class="lg:w-full border-rose-500 ">
+     <h1>helo</h1>
+      </div>
+    )
+  }
+  
